385434f8feab0c14ca9de2e07085adc7a9b933ce
AftabChowdhury <mmaftab.chowdhury@nttdata.com>
2025-04-11 16:44:56 +0200

    create job crud operations

---
+++ src/main/java/com/ac/firstjobapp/job/JobController.java
@@ -0,0 +1,51 @@
+package com.ac.firstjobapp.job;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class JobController {
+    private JobService jobService;
+
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping("/jobs")
+    public ResponseEntity<List<Job>> findAll() {
+        return ResponseEntity.ok(jobService.findAll());
+    }
+
+    @GetMapping("/jobs/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
+        Job job = jobService.getJobById(id);
+        if (job!=null) {
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping("/jobs")
+    public ResponseEntity<String> createJob(@RequestBody Job job){
+        jobService.createJob(job);
+        return new ResponseEntity<>("Successfully Created", HttpStatus.CREATED);
+    }
+
+    @PutMapping("/jobs/{id}")
+    public ResponseEntity<String> updateJob(@RequestBody Job job, @PathVariable Long id){
+        boolean isUpdated = jobService.updateJob(id, job);
+        if (isUpdated) return new ResponseEntity<>("Updated Successfully", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @DeleteMapping("/jobs/{id}")
+    public ResponseEntity<String> deleteJob(@PathVariable Long id){
+        boolean isDeleted = jobService.deleteJob(id);
+        if (isDeleted) return new ResponseEntity<>("Deleted Successfully", HttpStatus.OK);
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/ac/firstjobapp/job/JobService.java
@@ -0,0 +1,11 @@
+package com.ac.firstjobapp.job;
+
+import java.util.List;
+
+public interface JobService {
+    List<Job> findAll();
+    Job getJobById(Long id);
+    void createJob(Job job);
+    boolean deleteJob(Long id);
+    boolean updateJob(Long id, Job job);
+}
